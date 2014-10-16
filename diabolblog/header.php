7272a03c40ee5fa7fe5b3ded2f14563b8b1d6426
Nelson Alvarado <nelson@diabol.se>
2014-10-16 06:51:29 +0200

    Worked with the link to main site. Added page logo.

--- diabolblog/header.php
+++ diabolblog/header.php
@@ -42,9 +42,15 @@
 
 	<header id="masthead" class="site-header" role="banner">
 		<div class="header-main">
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+
+			<img src="<?php echo esc_url( home_url( '/' ) ); ?>wp-content/themes/diabolblog/images/diabolblog-logo.png" alt="<?php bloginfo( 'name' ); ?>">
 
 			<!--
+			wigth 254
+			height 37
+			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+
+			
 			<div class="search-toggle">
 				<a href="#search-container" class="screen-reader-text"><?php _e( 'Search', 'twentyfourteen' ); ?></a>
 			</div>
@@ -56,14 +62,14 @@
 				<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu' ) ); ?>
 			</nav>
 
-		</div>
+			<?php
+				$description = get_bloginfo( 'description', 'display' );
+				if ( ! empty ( $description ) ) :
+			?>
+			<a class="site-description" href="http://www.diabol.se"><?php echo esc_html( $description ); ?></a>
+			<?php endif; ?>
 
-		<?php
-			$description = get_bloginfo( 'description', 'display' );
-			if ( ! empty ( $description ) ) :
-		?>
-		<h2 class="site-description"><?php echo esc_html( $description ); ?></h2>
-		<?php endif; ?>
+		</div>
 		
 		<div id="search-container" class="search-box-wrapper hide">
 			<div class="search-box">
